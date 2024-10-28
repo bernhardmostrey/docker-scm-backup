5474578fd46a2f6d72be33fed63668339f6fcafb
Bernhard Mostrey <bmostrey@gmail.com>
2024-10-28 10:19:59 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-yq e -i '(.sources[] | select(.title == "github").name) = env(GH_ORG)' settings.yml
-yq e -i '(.sources[] | select(.title == "github").authName) = env(GH_USER)' settings.yml
-yq e -i '(.sources[] | select(.title == "github").password) = env(GH_PASS)' settings.yml
+yq e -i '(.sources[] | select(.title == env(TITLE1)).name) = env(TEAM1)' settings.yml
+yq e -i '(.sources[] | select(.title == env(TITLE1)).authName) = env(BB_USER)' settings.yml
+yq e -i '(.sources[] | select(.title == env(TITLE1)).password) = env(BB_PASSWORD)' settings.yml
+
+yq e -i '(.sources[] | select(.title == env(TITLE2)).name) = env(TEAM2)' settings.yml
+yq e -i '(.sources[] | select(.title == env(TITLE2)).authName) = env(BB_USER)' settings.yml
+yq e -i '(.sources[] | select(.title == env(TITLE2)).password) = env(BB_PASSWORD)' settings.yml
 
 exec dotnet ScmBackup.dll
